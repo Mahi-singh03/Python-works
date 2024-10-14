0ea4689dbdb4db38421a3ea3a573cece33df2681
mahi singh <aka.mahisingh@gmail.com>
2024-10-14 19:54:49 +0530

    HI

--- course_2/5 list - 5/code.py
+++ course_2/5 list - 5/code.py
@@ -13,7 +13,7 @@ print("the item at the index 3 Is : ",list1[3])
 
 # Example of -ve indexing :
 list2 = [10, 20, 30, 40]
-print("the item at the index -2 Is : ",list1[-2])   # return the 2nd last item of the list 
+print("the item at the index -2 Is : ",list1[-2])   # return the 2nd last item of the lists 
 
 
 
